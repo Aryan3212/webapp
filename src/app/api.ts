7157bb76afeb63d81cd3ef904ab5d2a0cec62258
Aryan Rahman <rahman.aryan07@gmail.com>
2023-08-30 23:29:20 +0600

    add api to get job details

--- src/app/api.ts
+++ src/app/api.ts
@@ -4,3 +4,7 @@ export async function getJobs() {
     const response = await fetch(`${baseUrl}/jobs/`)
     return response.json()
 }
+export async function getJob(jobId) {
+    const response = await fetch(`${baseUrl}/jobs/${jobId}`)
+    return response.json()
+}
