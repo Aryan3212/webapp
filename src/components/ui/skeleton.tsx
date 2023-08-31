845271cb11a1f50d986e2689473bb551eef7b219
Aryan Rahman <rahman.aryan07@gmail.com>
2023-09-01 01:52:28 +0600

    enhance: skeleton has a spinner loader now

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,14 +1,16 @@
 import { cn } from "@/lib/utils"
-
+import { ShadowInnerIcon } from "@radix-ui/react-icons"
 function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      className={cn("animate-pulse rounded-md bg-primary/20 flex justify-center items-center", className)}
       {...props}
-    />
+    >
+      <ShadowInnerIcon height={30} width={30} className="animate-spin background-muted"/>
+    </div>
   )
 }
 
