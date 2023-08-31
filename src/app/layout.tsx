5b2b037fcfc0eb807a9610f1b53d949a202b6b03
Aryan Rahman <rahman.aryan07@gmail.com>
2023-08-31 16:21:55 +0600

    feat: used file based metadata for robots.txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,19 +16,6 @@ export const metadata: Metadata = {
     title: 'WorkList',
     description: 'Fulfilling work for everyone. Find jobs here!',
   },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
 }
 
 export default function RootLayout({
